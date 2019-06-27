a2f951136885f2aa7f02f8ba51fa8182cd95360f
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-06-27 12:13:39 +0300

    убрал лишнее условие + var classList для сокращ.

--- js/filter.js
+++ js/filter.js
@@ -59,21 +59,21 @@ window.initSlider = function () {
   effects.addEventListener('click', effectsListener);
   sliderPin.addEventListener('mousedown', sliderPinListener);
 };
-// ничего не могу придумать с imageWrap.classList.contains
+
+var classList = imageWrap.classList;
+
 var changeSaturation = function (intensityIndex) {
-  if (imageWrap.classList.contains('effects__preview--chrome')) {
+  if (classList.contains('effects__preview--chrome')) {
     imageWrap.style.filter = 'grayscale(' + intensityIndex + ')';
-  } else if (imageWrap.classList.contains('effects__preview--sepia')) {
+  } else if (classList.contains('effects__preview--sepia')) {
     imageWrap.style.filter = 'sepia(' + intensityIndex + ')';
-  } else if (imageWrap.classList.contains('effects__preview--marvin')) {
+  } else if (classList.contains('effects__preview--marvin')) {
     imageWrap.style.filter = 'invert(' + (intensityIndex * 100) + '%)';
-  } else if (imageWrap.classList.contains('effects__preview--phobos')) {
+  } else if (classList.contains('effects__preview--phobos')) {
     imageWrap.style.filter = 'blur(' + (intensityIndex * 3) + 'px)';
-  } else if (imageWrap.classList.contains('effects__preview--heat')) {
+  } else if (classList.contains('effects__preview--heat')) {
     imageWrap.style.filter = 'brightness(' + (1 + intensityIndex * 2) + ')';
-  } else if (imageWrap.classList.contains('effects__preview--chrome')) {
-    imageWrap.style.filter = 'grayscale(' + intensityIndex + ')';
-  } else if (imageWrap.classList.contains('effects__preview--none')) {
+  } else if (classList.contains('effects__preview--none')) {
     imageWrap.style.filter = '';
   }
 };
