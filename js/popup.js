e98e83bfdd5998456de93f492b559103d896139c
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-07-26 16:41:52 +0300

    Валидация комментов и хештегов

--- js/popup.js
+++ js/popup.js
@@ -20,10 +20,63 @@
     openUploadPreview();
   };
 
-  var textDescription = document.querySelector('.text__description');
+  var commentInput = document.querySelector('.text__description');
+
+  commentInput.addEventListener('invalid', function () {
+    if (commentInput.validity.tooLong) {
+      commentInput.setCustomValidity('Длина комментария не должна превышать 140 символов');
+    }
+  });
+
+  var hashtagInput = document.querySelector('.text__hashtags');
+
+  hashtagInput.addEventListener('input', function () {
+    var hashtagError = validateHashtags(hashtagInput.value);
+    hashtagInput.setCustomValidity(hashtagError);
+  });
+
+  var validateHashtags = function (userInput) {
+    if (userInput === '') {
+      return '';
+    }
+
+    var arrayHashtags = userInput.toLowerCase().split(' ');
+
+    if (arrayHashtags.length > 5) {
+      return 'Вы не можете использовать больше 5 хэштегов';
+    }
+
+    for (var i = 0; i < arrayHashtags.length; i++) {
+      var hashtag = arrayHashtags[i];
+
+      if (hashtag[0] !== '#') {
+        return 'Вы забыли поставить знак #';
+      }
+
+      if (hashtag === '#') {
+        return ' Вы не ввели текст хэштэга';
+      }
+
+      var cutHashtag = hashtag.slice(1);
+
+      if (cutHashtag.indexOf('#') !== -1) {
+        return 'Вы забыли поставить пробел между хэштегами';
+      }
+
+      if (arrayHashtags.indexOf(hashtag) !== i) {
+        return 'Вы уже использовали данный хэштег';
+      }
+
+      if (hashtag.length > 20) {
+        return 'Длина хэштега должна быть не больше 20 символов, включая решётку';
+      }
+    }
+
+    return '';
+  };
 
   var onUploadPreviewEscPress = function (evt) {
-    if (evt.keyCode === ESC_KEYCODE && textDescription !== document.activeElement) {
+    if (evt.keyCode === ESC_KEYCODE && !commentInput.matches(':focus') && !hashtagInput.matches(':focus')) {
       closeUploadPreview();
     }
   };
